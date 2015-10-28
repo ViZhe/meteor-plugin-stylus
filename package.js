3f71aa2cda3d4b98d2dc5f789839c8835c06a83d
ViZhe <vizhe.cat@ya.ru>
2015-10-29 00:16:00 +0300

    v0.0.6

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 
 Package.describe({
     name: 'vizhe:stylus',
-    version: '0.0.5',
+    version: '0.0.6',
     summary: 'Stylus + url + autoprefixer + postcss-svg + postcss-zindex',
     git: 'https://github.com/hoppas/meteor-plugin-stylus.git',
     documentation: 'README.md'
