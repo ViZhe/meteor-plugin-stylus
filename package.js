c9a744b84ca18d34642e77354c324735050600b8
ViZhe <vizhe.cat@ya.ru>
2015-11-04 19:52:37 +0300

    0.3.3 - fast fix test svg path

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 
 Package.describe({
     name: 'vizhe:stylus',
-    version: '0.3.2',
+    version: '0.3.3',
     summary: 'Stylus + url + autoprefixer + postcss-svg + postcss-zindex',
     git: 'https://github.com/hoppas/meteor-plugin-stylus.git',
     documentation: 'README.md'
